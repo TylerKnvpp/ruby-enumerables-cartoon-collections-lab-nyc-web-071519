026811a231839e941c43b44ce426f55a64afa93d
tyler knapp <tgknapp11@gmail.com>
2019-07-04 20:32:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,11 +5,11 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |call| call.capitalize + '!' }
+  planeteer_calls.map { |c| c.capitalize + '!' }
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? { |call| call.length > 4 }
+  planeteer_calls.any? { |c| c.length > 4 }
 end
 
 def find_the_cheese(potentially_cheesy_items)
